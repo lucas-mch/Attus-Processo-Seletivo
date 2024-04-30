65b6c4de5fbe694fbc5fe97f32894977b9dbc7e0
Lucas Machado de Souza <lcsmachaado@gmail.com>
2024-04-30 14:34:37 -0300

    Initial commit

---
+++ src/main/java/dev/lucasmachado/attusprocessoseletivo/AttusProcessoSeletivoApplication.java
@@ -0,0 +1,13 @@
+package dev.lucasmachado.attusprocessoseletivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttusProcessoSeletivoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttusProcessoSeletivoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/lucasmachado/attusprocessoseletivo/AttusProcessoSeletivoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lucasmachado.attusprocessoseletivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttusProcessoSeletivoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
