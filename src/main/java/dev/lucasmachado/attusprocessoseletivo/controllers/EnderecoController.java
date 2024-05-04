9562cfd57dfdaaf4e9abf9ca55a0dfa75e3c3cbb
Lucas Machado de Souza <lcsmachaado@gmail.com>
2024-05-04 20:11:06 -0300

    Tests -> First TDD layer implemented.

--- src/main/java/dev/lucasmachado/attusprocessoseletivo/controllers/EnderecoController.java
+++ src/main/java/dev/lucasmachado/attusprocessoseletivo/controllers/EnderecoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("endereco")
+@RequestMapping("/enderecos")
 public class EnderecoController {
 
     @RequestMapping(method = RequestMethod.GET)
