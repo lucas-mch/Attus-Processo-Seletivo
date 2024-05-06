4cd9f4d537dce12b5464b94e749109c7d3b19f3f
Lucas Machado de Souza <lcsmachaado@gmail.com>
2024-05-06 01:22:15 -0300

    Impl - @Transactional annotation is necessary for UPDATE operations.

--- src/main/java/dev/lucasmachado/attusprocessoseletivo/repositories/EnderecoRepository.java
+++ src/main/java/dev/lucasmachado/attusprocessoseletivo/repositories/EnderecoRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,6 +18,7 @@ public interface EnderecoRepository extends JpaRepository<Endereco,Long> {
     List<Endereco> findAllFromPessoas(List<Long> ids);
 
     @Modifying
+    @Transactional
     @Query("UPDATE Endereco adress SET adress.isPrincipal = false where adress.pessoa.id = :id")
     void resetEnderecoPrincipal(Long id);
 
