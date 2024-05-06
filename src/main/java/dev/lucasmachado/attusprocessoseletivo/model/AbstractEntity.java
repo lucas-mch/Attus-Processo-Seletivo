db8864a70ecb517f718877ca42acdfd236921ebf
Lucas Machado de Souza <lcsmachaado@gmail.com>
2024-05-06 01:18:52 -0300

    refact - Removing useless code.

--- src/main/java/dev/lucasmachado/attusprocessoseletivo/model/AbstractEntity.java
+++ src/main/java/dev/lucasmachado/attusprocessoseletivo/model/AbstractEntity.java
@@ -2,53 +2,23 @@ package dev.lucasmachado.attusprocessoseletivo.model;
 
 import jakarta.persistence.*;
 
-import java.io.Serializable;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
 import java.util.Objects;
 
 @MappedSuperclass
-public abstract class AbstractEntity<Entity> implements IEntity<Entity> {
+public abstract class AbstractEntity<Entity> implements IEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Version
-    @Column(name = "version")
-    private int version;
-    @Column(name="created_at")
-    private Timestamp createdAt;
-    @Column(name="updated_at")
-    private Timestamp updatedAt;
 
     public AbstractEntity() {
-        this.updatedAt = Timestamp.valueOf(LocalDateTime.now());
     }
 
     public AbstractEntity(Long id) {
         this.id = id;
-        this.createdAt = Timestamp.valueOf(LocalDateTime.now());
-        this.updatedAt = Timestamp.valueOf(LocalDateTime.now());
     }
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
-    public int getVersion() { return version; }
-    public void setVersion(int version) { this.version = version; }
-    public Timestamp getCreatedAt() { return createdAt; }
-    public Timestamp getUpdatedAt() { return updatedAt; }
-    public void setUpdatedAt(Timestamp updatedAt) { this.updatedAt = updatedAt; }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        AbstractEntity that = (AbstractEntity) o;
-        return id.equals(that.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 
 }
 
