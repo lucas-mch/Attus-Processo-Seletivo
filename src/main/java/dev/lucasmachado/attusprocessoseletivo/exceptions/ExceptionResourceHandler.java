177b1afd7256e03481e39847765ac1acd95fac91
Lucas Machado de Souza <lcsmachaado@gmail.com>
2024-05-06 01:20:45 -0300

    Impl - Expection Handler for Controllers

--- src/main/java/dev/lucasmachado/attusprocessoseletivo/exceptions/ExceptionResourceHandler.java
+++ src/main/java/dev/lucasmachado/attusprocessoseletivo/exceptions/ExceptionResourceHandler.java
@@ -5,6 +5,8 @@ import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -14,17 +16,20 @@ import java.util.NoSuchElementException;
 public class ExceptionResourceHandler {
 
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<StandardError> objectNotFound(NoSuchElementException e, HttpServletRequest request){
+    public ResponseEntity<StandardError> objectNotFound(NoSuchElementException e, HttpServletRequest request) {
         StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
     }
 
-//   ConstraintViolationException
-@ExceptionHandler(ConstraintViolationException.class)
-public ResponseEntity<StandardError> objectNotFound(ConstraintViolationException e, HttpServletRequest request){
-    ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(),"Validation error!", System.currentTimeMillis());
-    e.getConstraintViolations().stream().forEach(x -> err.addError(new FieldMessage(x.getPropertyPath().toString(),x.getMessage())));
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
-}
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> dataIntegrity(MethodArgumentNotValidException e, HttpServletRequest request) {
+        ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Validation error!", System.currentTimeMillis());
+
+        for (FieldError x : e.getBindingResult().getFieldErrors()) {
+            err.addError(new FieldMessage(x.getField(), x.getDefaultMessage()));
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
 
 }
